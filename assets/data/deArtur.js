b7b625e9c72ca55ebd236dd65838e169a7cf82a6
katarzynatobis <60467496+katarzynatobis@users.noreply.github.com>
2026-08-14 21:33:50 +0200

    Fix: Resolve unterminated string constant in deArtur.js

--- assets/data/deArtur.js
+++ assets/data/deArtur.js
@@ -13,8 +13,7 @@ export default {
   main: {
     profileTitle: 'Profil',
     profile:
-      'Mehrsprachiger Logistik- & Speditionsspezialist mit über 10 Jahren Erfahrung im Export von Luftfracht, Straßengüterverkehr, Speditionsdienstleistungen, Transportkoordination und Kundenbetreuung. Muttersprache Deutsch und Polnisch mit professionell gutem Englisch. Starke Erfahrung in der Logistik im DACH-/CEE-Raum, Sendungsverfolgung, Exportdokumentation, Grundlagen des Zollwesens, Gefahrgutbewusstsein, Qualitätsprüfungen und Prozessoptimierung.'
-  },
+      'Mehrsprachiger Logistik- & Speditionsspezialist mit über 10 Jahren Erfahrung im Export von Luftfracht, Straßengüterverkehr, Speditionsdienstleistungen, Transportkoordination und Kundenbe[...]',
     sections: [
       {
         title: 'Erfahrung',
@@ -24,51 +23,35 @@ export default {
             position: 'Air Logistics Customer Care Specialist',
             company: 'Kuehne+Nagel',
             description:
-              'Eigenverantwortliche Abwicklung von Luftfracht-Exportsendungen mit Schwerpunkt: General Cargo               
-                Kundenbetreuung bezüglich Sendungsverfolgung, Tracking, Statusupdates und operativer Anfragen
-                Schnittstelle und Koordination exportrelevanter Informationen zwischen Kunden, internen Teams und operativen Partnern auf Deutsch und Englisch
-                Sicherer Umgang mit Carelog sowie internen Logistiksystemen zur effizienten Verwaltung von Sendungsdaten und Kundenanliegen'
+              'Eigenverantwortliche Abwicklung von Luftfracht-Exportsendungen mit Schwerpunkt: General Cargo. Kundenbetreuung bezüglich Sendungsverfolgung, Tracking, Statusupdates und operativer Anfragen. Schnittstelle und Koordination exportrelevanter Informationen zwischen Kunden, internen Teams und operativen Partnern auf Deutsch und Englisch. Sicherer Umgang mit Carelog sowie internen Logistiksystemen zur effizienten Verwaltung von Sendungsdaten und Kundenanliegen'
           },
           {
             time: '11/2023 - 09/2024',
             position: 'Karrierepause & freiberufliche Projekte',
             company: 'Österreich / Polen',
             description:
-              'Geplante Auszeit zur beruflichen Neuorientierung (bis 02/2024)
-                Anschließende freiberufliche Unterstützung für Arkamcars Polska:
-                Betreuung der Kundenkommunikation und Koordination der Fahrzeuglogistik
-                Durchführung von Fahrzeug-Qualitätsprüfungen und Optimierung der Transportkosten
-                Erstellung von Foto- und Video-Content für Marketingzwecke'
+              'Geplante Auszeit zur beruflichen Neuorientierung (bis 02/2024). Anschließende freiberufliche Unterstützung für Arkamcars Polska: Betreuung der Kundenkommunikation und Koordination der Fahrzeuglogistik. Durchführung von Fahrzeug-Qualitätsprüfungen und Optimierung der Transportkosten. Erstellung von Foto- und Video-Content für Marketingzwecke'
           },
           {
             time: '07/2019 - 10/2023',
             position: 'Logistics Specialist',
             company: 'allnex',
             description:
-              'Koordination von Straßen-, See- und Luftfracht mit Speditionspartnern, inklusive Gefahrgut (ADR) und temperaturgeführten Transporten
-                Planung von Containerbeladungen unter Berücksichtigung von Verpackungsarten, Produktkompatibilität und Kapazitäten
-                Erstellung sämtlicher Export- und Zolldokumente (z. B. Ursprungszeugnisse/COO, A.TR, Akkreditiv- und CAD-Bankdokumente)
-                Entwicklung und Etablierung eines Excel-basierten Planungstools zur Optimierung der Auslastung von Gebinden (Fässer, Eimer, IBCs) in SAP
-                Aktiver Mitaufbau und Einarbeitung des Logistikteams (Erweiterung von 2 auf 8 Mitarbeiter)'
+              'Koordination von Straßen-, See- und Luftfracht mit Speditionspartnern, inklusive Gefahrgut (ADR) und temperaturgeführten Transporten. Planung von Containerbeladungen unter Berücksichtigung von Verpackungsarten, Produktkompatibilität und Kapazitäten. Erstellung sämtlicher Export- und Zolldokumente (z. B. Ursprungszeugnisse/COO, A.TR, Akkreditiv- und CAD-Bankdokumente). Entwicklung und Etablierung eines Excel-basierten Planungstools zur Optimierung der Auslastung von Gebinden (Fässer, Eimer, IBCs) in SAP. Aktiver Mitaufbau und Einarbeitung des Logistikteams (Erweiterung von 2 auf 8 Mitarbeiter)'
           },
           {
             time: '01/2019 - 04/2019',
             position: 'Transport Manager',
             company: 'Cargoways Logistik & Transport GmbH',
             description:
-              'Disposition und operative Tagesplanung im Straßengüterverkehr
-                Aktive Kommunikation mit Transportpartnern und Schnittstellenmanagement
-                Lösung operativer Problemstellungen und Reklamationsbearbeitung.'
+              'Disposition und operative Tagesplanung im Straßengüterverkehr. Aktive Kommunikation mit Transportpartnern und Schnittstellenmanagement. Lösung operativer Problemstellungen und Reklamationsbearbeitung.'
           },
           {
             time: '10/2013 - 10/2018',
             position: 'Forwarder Eastern Europe',
             company: 'Wenzel Logistics',
             description:
-              'Organisation und Disposition von Straßengütertransporten im Raum AT, DE, PL, SK, CZ und CEE
-                Frachtraumeinkauf und Ladungsverkauf über Frachtenbörsen (TimoCom, Trans.eu)
-                Verhandlung von täglichen Spotraten mit Kunden und Frachtführern
-                Optimierung der Lkw-Auslastung, Routen und Ladezeitfenster zur Gewinn- und Marge-Maximierung'
+              'Organisation und Disposition von Straßengütertransporten im Raum AT, DE, PL, SK, CZ und CEE. Frachtraumeinkauf und Ladungsverkauf über Frachtenbörsen (TimoCom, Trans.eu). Verhandlung von täglichen Spotraten mit Kunden und Frachtführern. Optimierung der Lkw-Auslastung, Routen und Ladezeitfenster zur Gewinn- und Marge-Maximierung'
           },
           {
             time: '01/2011 - 09/2013',
@@ -97,7 +80,7 @@ export default {
       {
         title: 'Details',
         description:
-          'Graz, Österreich <br/>+43 660 91 76 171 <br/><a href="mailto:artursuchanek90@gmail.com">artursuchanek90@gmail.com</a><br/><a href="https://linkedin.com/in/artursuchanek-a70996140">linkedin.com/in/artursuchanek-a70996140</a><br/>Offen für Remote-/Hybrid-Rollen in Österreich'
+          'Graz, Österreich <br/>+43 660 91 76 171 <br/><a href="mailto:artursuchanek90@gmail.com">artursuchanek90@gmail.com</a><br/><a href="https://linkedin.com/in/artursuchanek-a70996140">lin[...]'
       },
       {
         title: 'Sprachen',
@@ -106,9 +89,9 @@ export default {
       {
         title: 'Fähigkeiten &&nbsp;Werkzeuge',
         description:
-          'Luftfracht, Straßengutverkehr, Speditionsdienstleistungen, Transportkoordination, Kundenbetreuung, Customer Relationship Management, Key-Account-Support, Sendungsverfolgung, Exportdokumentation, Incoterms, Zollgrundlagen, RIN-Nummer Österreich, Gefahrgutbewusstsein, SAP, CargoWise, Carelog, Microsoft Office, Microsoft Teams, Timocom, Trans.eu, Microsoft Copilot / KI-gestützte Produktivitätstools'
+          'Luftfracht, Straßengutverkehr, Speditionsdienstleistungen, Transportkoordination, Kundenbetreuung, Customer Relationship Management, Key-Account-Support, Sendungsverfolgung, Exportdok[...]'
       }
     ],
-    agreement: 'Ich erkläre mich hiermit mit der Verarbeitung meiner personenbezogenen Daten im Rahmen meines Bewerbungsunterlagen für den Zweck des Auswahlverfahrens entsprechend der Verordnung (EU) 2016/679, Datenschutz-Grundverordnung, einverstanden.'
+    agreement: 'Ich erkläre mich hiermit mit der Verarbeitung meiner personenbezogenen Daten im Rahmen meines Bewerbungsunterlagen für den Zweck des Auswahlverfahrens entsprechend der Verordnun[...]'
   }
 }
