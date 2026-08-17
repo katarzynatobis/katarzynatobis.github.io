ad27945beedfbab96c8e656af43437fb81a70c02
artursuchanek90 <artursuchanek90@gmail.com>
2026-08-17 12:43:01 +0200

    Update deArtur.js

--- assets/data/deArtur.js
+++ assets/data/deArtur.js
@@ -80,7 +80,7 @@ export default {
       {
         title: 'Details',
         description:
-          'Graz, Österreich <br/>+43 660 91 76 171 <br/><a href="mailto:artursuchanek90@gmail.com">artursuchanek90@gmail.com</a><br/><a href="https://linkedin.com/in/artursuchanek-a70996140">linkedin.com/in/artursuchanek-a70996140'
+          'Graz, Österreich <br/>+43 660 91 76 171 <br/><a href="mailto:artursuchanek90@gmail.com">artursuchanek90@gmail.com</a><br/><a href="https://www.linkedin.com/in/artursuchanek90'
       },
       {
         title: 'Sprachen',
